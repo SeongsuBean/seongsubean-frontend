556030c27024720a86e51eea2e47c27a92856b14
mdsihmaro <lsh11946@naver.com>
2025-07-13 15:41:53 +0900

    설정

---
+++ src/main/java/com/oopsw/seongsubeanfrontend/SeongsubeanFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.oopsw.seongsubeanfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeongsubeanFrontendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeongsubeanFrontendApplication.class, args);
+  }
+
+}
